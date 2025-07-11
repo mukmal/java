84af22891eb53f13a7fe7b251c81a57a9379c8c0
mukmal <mukesh@DESKTOP-MNCGO64.localdomain>
2025-07-11 11:02:22 +0000

    classes and objects

---
+++ daily_codes/classes_objects/program21.java
@@ -0,0 +1,23 @@
+import company.TechInfo;
+import employee.EmpInfo;
+class Demo {
+	public static void main(String[] args) {
+		TechInfo ti = new TechInfo();
+		ti.techStack();
+		EmpInfo ei = new EmpInfo();
+		System.out.println(ei.empId);
+		System.out.println(ei.empName);
+		System.out.println(ei.empSal);
+		System.out.println(ei.devType);
+	}
+}
+
+
+//webDev
+//BackendDev
+//MobileDev
+//Testing
+//10
+//Kanha
+//1.5
+//MobileDev
